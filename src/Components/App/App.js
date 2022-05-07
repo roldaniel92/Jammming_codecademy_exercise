7e55d13a76f89be3293be02d46d82a5724dbb96d
Roldaniel <danielf_duartem@hotmail.com>
2022-05-07 17:33:45 -0500

    begin Obtain a Spotify Access Token

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -21,6 +21,9 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track){
@@ -49,18 +52,29 @@ class App extends React.Component {
     });
   }
 
+  savePlaylist(){
+    let trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
+  search(term){
+    console.log(term);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar/>
+          <SearchBar onSearch = {this.search}/>
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
             <PlayList 
             playlistName={this.state.playlistName} 
             playlistTracks={this.state.playlistTracks}
-            onRemove = {this.removeTrack}/>
+            onRemove = {this.removeTrack}
+            onNameChange = {this.updatePlaylistName}
+            onSave = {this.savePlaylist}
+            />
           </div>
         </div>
       </div>
